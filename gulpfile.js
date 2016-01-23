f64b5653970ba76462d9715362c056326c892da1
Aidi Stan <aidistan@live.com>
2016-01-23 14:39:31 +0800

    Rename scripts

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,18 @@ var cp = require('child_process');
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 
-gulp.task('pre', function() {
-  cp.execSync('npm run build');
+gulp.task('bundle', function() {
+  cp.execSync('npm run bundle');
 });
 
-gulp.task('js', ['pre'], function() {
+gulp.task('js', ['bundle'], function() {
   return gulp.src('./dist/bragi.js')
     .pipe(require('gulp-uglify')())
     .pipe(rename({extname: '.min.js'}))
     .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('css', ['pre'], function(){
+gulp.task('css', ['bundle'], function(){
  return gulp.src('./dist/bragi.css')
    .pipe(require('gulp-minify-css')())
    .pipe(rename({extname: '.min.css'}))
